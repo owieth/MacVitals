4534d9f8d3b39ed01c0b964d10df0679e77e51d4
Olivier <olivier.winkler@icloud.com>
2026-03-17 08:38:30 +0100

    chore: remove dead code (defaultRefreshInterval, batteryIOMainPort)
    Remove Constants.defaultRefreshInterval (never referenced; actual default
    is RefreshRate.twoSeconds) and the unused batteryIOMainPort alias in
    BatteryCollector.
    
    Closes #37

--- MacVitals/MacVitals/Utilities/Constants.swift
+++ MacVitals/MacVitals/Utilities/Constants.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 enum Constants {
-    static let defaultRefreshInterval: TimeInterval = 2.0
     static let popoverWidth: CGFloat = 420
     static let popoverHeight: CGFloat = 550
     static let appName = "MacVitals"
